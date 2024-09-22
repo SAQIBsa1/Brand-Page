40d2313f1cf85b4c52b45279fa29c0da29366a5c
SAQIBsa1 <saqibsayeed8127@gmail.com>
2024-09-22 14:24:39 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+const HeroSection = () => {
+    return (
+     <main className="hero container">
+        <div className="hero-content">
+            
+            <h1>YOUR FEET DESERVES THE BEST</h1>
+            <p>YOUR FEET DESERVES THE BEST,YOUR FEET DESERVES THE BEST,YOUR FEET DESERVES THE BEST,YOUR FEET DESERVES THE BEST</p>
+        <div className="hero-btn">
+        <button>Shop Now</button>
+        <button className="secondary-btn">Category</button>
+
+        </div>
+        <div className="shopping">
+            <p>Also Available on</p>
+        <div className="brand-icons">
+            <img src="/images/download1.png" alt="amazon" />
+            <img src="/images/images.png" alt="flipkart" />
+        </div>
+
+        </div>
+        
+        </div>
+        <div className="hero-image"></div>
+        <img src="/images/images.jpeg" alt="images" />
+
+    </main>
+    );
+};
+
+
+export default HeroSection;
